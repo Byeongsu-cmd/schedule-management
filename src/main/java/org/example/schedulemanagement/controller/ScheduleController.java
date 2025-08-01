df8fcfd70d1eb09fa979319635aa0351a919ec06
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-01 20:09:22 +0900

    이미지 오류 수정

--- src/main/java/org/example/schedulemanagement/controller/ScheduleController.java
+++ src/main/java/org/example/schedulemanagement/controller/ScheduleController.java
@@ -1,4 +1,24 @@
 package org.example.schedulemanagement.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.schedulemanagement.dto.ScheduleRequest;
+import org.example.schedulemanagement.dto.ScheduleResponse;
+import org.example.schedulemanagement.service.ScheduleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+
+    @PostMapping("/schedules")
+    public ResponseEntity<ScheduleResponse> createSchedule(
+            @RequestBody ScheduleRequest scheduleRequest
+    ) {
+        return ResponseEntity.ok(scheduleService.createSchedule(scheduleRequest));
+    }
 }
--- src/main/java/org/example/schedulemanagement/dto/ScheduleResponse.java
+++ src/main/java/org/example/schedulemanagement/dto/ScheduleResponse.java
@@ -7,13 +7,13 @@ import java.time.LocalDateTime;
 @Getter
 public class ScheduleResponse {
 
-    private Long id;
-    private String title; // 일정 제목
-    private String description; // 일정 내용
-    private String userName; // 작성자명
-    private String password; // 비밀번호
-    private LocalDateTime createTime; // 작성일
-    private LocalDateTime updateTime; // 수정일
+    private final Long id;
+    private final String title; // 일정 제목
+    private final String description; // 일정 내용
+    private final String userName; // 작성자명
+    private final String password; // 비밀번호
+    private final LocalDateTime createTime; // 작성일
+    private final LocalDateTime updateTime; // 수정일
 
     public ScheduleResponse(Long id, String title, String description, String userName, String password, LocalDateTime createTime, LocalDateTime updateTime) {
         this.id = id;
